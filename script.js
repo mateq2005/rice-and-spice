edd044e2ef1f3a1ff2a8ae98793162c1d03f31b2
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-11 18:56:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardConatiner = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+
+fetch(`https://mateq2005.github.io/rice-and-spice/resources/recipes.json`)
+    .then(res => res.json())
+    .then(data => {
+        users = data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0];
+            const image = card.querySelector("[data-image]");
+            const header = card.querySelector("[data-header]");
+            const body = card.querySelector("[data-body]");
+            image.src = user.image;
+            header.textContent = user.name;
+            body.textContent = user.description;
+            userCardConatiner.append(card);
+            return {
+                image: user.image,
+                name: user.name,
+                description: user.description,
+                element: card 
+            }
+        });
+    });
