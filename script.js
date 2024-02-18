3231164f334daea047494588851b388db5a35bcf
Mateusz <mateusz.banaszczyk.dg@gmail.com>
2024-02-18 21:28:02 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,70 @@ const userCardTemplate = document.querySelector("[data-user-template]");
 const userCardConatiner = document.querySelector("[data-user-cards-container]");
 const searchInput = document.querySelector("[data-search]");
 
-fetch(`https://mateq2005.github.io/rice-and-spice/resources/recipes/breakfast/breakfast.json`)
+fetch(`https://mateq2005.github.io/rice-and-spice/resources/recipes/breakfast.json`)
+    .then(res => res.json())
+    .then(data => {
+        users = data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0];
+            const image = card.querySelector("[data-image]");
+            const header = card.querySelector("[data-header]");
+            const body = card.querySelector("[data-body]");
+            image.src = user.image;
+            header.textContent = user.name;
+            body.textContent = user.description;
+            userCardConatiner.append(card);
+            return {
+                image: user.image,
+                name: user.name,
+                description: user.description,
+                element: card 
+            }
+        });
+    });
+
+fetch(`https://mateq2005.github.io/rice-and-spice/resources/recipes/lunch.json`)
+    .then(res => res.json())
+    .then(data => {
+        users = data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0];
+            const image = card.querySelector("[data-image]");
+            const header = card.querySelector("[data-header]");
+            const body = card.querySelector("[data-body]");
+            image.src = user.image;
+            header.textContent = user.name;
+            body.textContent = user.description;
+            userCardConatiner.append(card);
+            return {
+                image: user.image,
+                name: user.name,
+                description: user.description,
+                element: card 
+            }
+        });
+    });
+
+fetch(`https://mateq2005.github.io/rice-and-spice/resources/recipes/dinner.json`)
+    .then(res => res.json())
+    .then(data => {
+        users = data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0];
+            const image = card.querySelector("[data-image]");
+            const header = card.querySelector("[data-header]");
+            const body = card.querySelector("[data-body]");
+            image.src = user.image;
+            header.textContent = user.name;
+            body.textContent = user.description;
+            userCardConatiner.append(card);
+            return {
+                image: user.image,
+                name: user.name,
+                description: user.description,
+                element: card 
+            }
+        });
+    });
+
+fetch(`https://mateq2005.github.io/rice-and-spice/resources/recipes/dessert.json`)
     .then(res => res.json())
     .then(data => {
         users = data.map(user => {
